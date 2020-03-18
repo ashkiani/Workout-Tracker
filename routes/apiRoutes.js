156561fa1512e754e24ee72b3e6b05c76f2528d7
Sia <ashkiani@yahoo.com>
2020-03-18 19:37:30 -0400

    Updated /api/workout so it saves the input data into the database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,14 @@ module.exports = function(app) {
   app.post("/api/workout", async (req, res) => {
     console.log("POST data received at /api/workout:");
     console.log(req.body);
-    console.log(res);
+    const newWorkout = new db.Workout(req.body);
+    try {
+      let dbRoutine = await db.Workout.create(newWorkout);
+      res.status(200).send(dbRoutine);
+    } catch (err) {
+      res.status(400).send(err._message);
+      console.log(err._message);
+    }
   });
 
   app.post("/api/exercise", async (req, res) => {
