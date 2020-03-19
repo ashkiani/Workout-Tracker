45fd37f0dbb2a0c573b44fa0a2a3efe5e4550eea
Sia <ashkiani@yahoo.com>
2020-03-18 20:54:04 -0400

    Updated /api/exercise route so it can save new exercises

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,10 +18,30 @@ module.exports = function(app) {
       console.log(err._message);
     }
   });
-
   app.post("/api/exercise", async (req, res) => {
     console.log("POST data received at /api/exercise:");
     console.log(req.body);
     console.log(res);
+
+    let exerciseInfo = {
+      name: req.body.excName,
+      reps: req.body.excReps
+    };
+    const newExercise = new db.Exercise(exerciseInfo);
+    try {
+      let addExercise = await db.Exercise.create(newExercise);
+      try {
+        let dbRoutine = await db.Workout.findOneAndUpdate(
+          { _id: req.body.workoutId },
+          { $push: { exercises: addExercise._id } },
+          { new: true }
+        );
+        res.status(200).send(dbRoutine);
+      } catch (err) {
+        res.status(400).send(err);
+      }
+    } catch (err) {
+      res.status(400).send(err);
+    }
   });
 };
